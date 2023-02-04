ac2a104709f18374bf3dd5dedda7c69886f96178
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-02-04 18:18:57 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchImages } from 'services/Api';
 import { ToastContainer, toast } from 'react-toastify';
 import Searchbar from './Searchbar';
@@ -7,111 +7,98 @@ import Button from './Batton/Button';
 import Modal from './Modal';
 import { ColorRing } from 'react-loader-spinner';
 import css from './App.module.css';
-class App extends Component {
-  state = {
-    search: '',
-    images: [],
-    loading: false,
-    error: null,
-    page: 1,
-    showModal: false,
-    largeImageURL: '',
-  };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { search, page } = this.state;
-    if (prevState.search !== search || prevState.page !== page) {
-      this.fetchImages();
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+
+  useEffect(() => {
+    if (!search) {
+      return;
     }
-  }
 
-  async fetchImages() {
-    try {
-      this.setState({ loading: true });
-      const { search, page } = this.state;
-      const items = await fetchImages(search, page);
+    const searchImages = async () => {
+      try {
+        setLoading(true);
+        const items = await fetchImages(search, page);
 
-      this.setState(({ images }) => ({
-        images: [...images, ...items],
-      }));
-    } catch (error) {
-      this.setState({ error: toast.error('Sorry try again latter') });
-    } finally {
-      this.setState({ loading: false, error: null });
-    }
-  }
+        setImages(prevImages => [...prevImages, ...items]);
+      } catch (error) {
+        setError(toast.error('Sorry try again latter'));
+      } finally {
+        setLoading(false);
+        setError(null);
+      }
+    };
+    searchImages();
+  }, [search, page]);
 
-  onImageClick = data => {
-    this.setState({
-      largeImageURL: data,
-      showModal: true,
-    });
+  const onImageClick = data => {
+    setLargeImageURL(data);
+    setShowModal(true);
   };
 
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-      largeImageURL: '',
-    });
+  const closeModal = () => {
+    setShowModal(false);
+    setLargeImageURL('');
   };
 
-  onSubmitForm = data => {
-    this.setState({ search: data, images: [], page: 1 });
+  const onSubmitForm = data => {
+    setSearch(data);
+    setImages([]);
+    setPage(1);
   };
 
-  loadMore = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { images, showModal, largeImageURL, loading, page, error } =
-      this.state;
-    const { onImageClick, loadMore, closeModal } = this;
-    return (
-      <>
-        <Searchbar onSubmit={this.onSubmitForm} />
-        {loading && page === 1 && (
-          <ColorRing
-            visible={true}
-            height="100"
-            width="100"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass={css.blocks_wrapper}
-            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-          />
-        )}
-
-        {images.length > 0 && (
-          <>
-            <ImageGallery images={images} onImageClick={onImageClick} />
-            {loading ? (
-              <ColorRing
-                visible={true}
-                height="100"
-                width="100"
-                ariaLabel="blocks-loading"
-                wrapperStyle={{}}
-                wrapperClass={css.blocks_wrapper}
-                colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-              />
-            ) : (
-              <Button loadMore={loadMore} title="Load more" />
-            )}
-          </>
-        )}
-        {error && <p>{error}</p>}
-
-        {showModal && (
-          <Modal close={closeModal}>
-            <img src={largeImageURL} alt="" />
-          </Modal>
-        )}
-
-        <ToastContainer autoClose={3000} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmitForm} />
+      {loading && page === 1 && (
+        <ColorRing
+          visible={true}
+          height="100"
+          width="100"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass={css.blocks_wrapper}
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />
+      )}
+      {images.length > 0 && (
+        <>
+          <ImageGallery images={images} onImageClick={onImageClick} />
+          {loading ? (
+            <ColorRing
+              visible={true}
+              height="100"
+              width="100"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass={css.blocks_wrapper}
+              colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            />
+          ) : (
+            <Button loadMore={loadMore} title="Load more" />
+          )}
+        </>
+      )}
+      {error && <p>{error}</p>}
+      {showModal && (
+        <Modal close={closeModal}>
+          <img src={largeImageURL} alt="" />
+        </Modal>
+      )}
+      <ToastContainer autoClose={3000} />
+    </>
+  );
+};
 
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,62 +1,103 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import { AiOutlineSearch } from 'react-icons/ai';
+import initialState from './initialState';
 import css from './Searchbar.module.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-class Searchbar extends Component {
-  state = {
-    search: '',
-  };
+const Searchbar = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...initialState });
 
-  handleOnSubmit = e => {
-    const { search } = this.state;
+  const handleOnSubmit = e => {
     e.preventDefault();
-    if (search.trim() === '') {
+    if (state.search.trim() === '') {
       toast.info('Nothing selected!');
       return;
     }
-    const { onSubmit } = this.props;
-    onSubmit(search);
-    this.reset();
+    onSubmit(state.search);
+    setState({ ...initialState });
   };
-  reset() {
-    this.setState({ search: '' });
-  }
-  handleChange = ({ target }) => {
+
+  const handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    setState({ [name]: value });
   };
 
-  render() {
-    const { search } = this.state;
-    const { handleOnSubmit, handleChange } = this;
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={handleOnSubmit}>
-          <button type="submit" className={css.SearchForm__button}>
-            <AiOutlineSearch className={css.SearchBtnIcon} />
-          </button>
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleOnSubmit}>
+        <button type="submit" className={css.SearchForm__button}>
+          <AiOutlineSearch className={css.SearchBtnIcon} />
+        </button>
 
-          <input
-            className={css.SearchForm__input}
-            type="text"
-            name="search"
-            value={search}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchForm__input}
+          type="text"
+          name="search"
+          value={state.search}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// class Searchbar extends Component {
+//   state = {
+//     search: '',
+//   };
+
+//   handleOnSubmit = e => {
+//     const { search } = this.state;
+//     e.preventDefault();
+//     if (search.trim() === '') {
+//       toast.info('Nothing selected!');
+//       return;
+//     }
+//     const { onSubmit } = this.props;
+//     onSubmit(search);
+//     this.reset();
+//   };
+//   reset() {
+//     this.setState({ search: '' });
+//   }
+//   handleChange = ({ target }) => {
+//     const { name, value } = target;
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     const { search } = this.state;
+//     const { handleOnSubmit, handleChange } = this;
+//     return (
+//       <header className={css.Searchbar}>
+//         <form className={css.SearchForm} onSubmit={handleOnSubmit}>
+//           <button type="submit" className={css.SearchForm__button}>
+//             <AiOutlineSearch className={css.SearchBtnIcon} />
+//           </button>
+
+//           <input
+//             className={css.SearchForm__input}
+//             type="text"
+//             name="search"
+//             value={search}
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={handleChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
